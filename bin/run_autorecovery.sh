84847425ceb2314bece158e33a4f8ce7ffc69208
gaozhangmin <gaozhangmin@kuaishou.com>
2025-11-07 04:12:57 +0000

    add run_autorecovery.sh
    KDev_MR_link：https://ksurl.cn/JScxWy9E

---
+++ bin/run_autorecovery.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+#/**
+# * Licensed to the Apache Software Foundation (ASF) under one
+# * or more contributor license agreements.  See the NOTICE file
+# * distributed with this work for additional information
+# * regarding copyright ownership.  The ASF licenses this file
+# * to you under the Apache License, Version 2.0 (the
+# * "License"); you may not use this file except in compliance
+# * with the License.  You may obtain a copy of the License at
+# *
+# *     http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# */
+
+# for debug
+set -x
+
+# install jdk
+bk_basic=/home/web_server/bk-autorecovery
+jdk_dirname=$bk_basic/jdk-17
+wget -q https://halo.corp.kuaishou.com/api/cloud-storage/v1/public-objects/bop/jdk-17_linux-x64_bin.tar.gz
+if [ ! -d "$bk_basic" ]; then
+  mkdir -p "$bk_basic"
+fi
+if [ -d "$jdk_dirname" ]; then
+  rm -rf "$jdk_dirname"
+fi
+
+tar -xzf jdk-17_linux-x64_bin.tar.gz
+mv jdk-17.* $jdk_dirname
+rm jdk-17_linux-x64_bin.tar.gz
+
+# link
+if [ -L "/home/web_server/bk-autorecovery/apps" ]; then
+    rm /home/web_server/bk-autorecovery/apps
+fi
+ln -s /data/web_server/project/kuaishou-runner-apps/"$KWS_SERVICE_NAME"/code/bk-autorecovery /home/web_server/bk-autorecovery/apps
+
+if [ -d "/home/web_server/bk-autorecovery/supervisord" ]; then
+    rm /home/web_server/bk-autorecovery/supervisord
+fi
+ln -s /data/web_server/supervisord/conf /home/web_server/bk-autorecovery/supervisord
+
+## save ENV vars
+echo "export KWS_SERVICE_NAME=$KWS_SERVICE_NAME" > /data/web_server/project/kuaishou-runner-apps/"$KWS_SERVICE_NAME"/code/bk-autorecovery/conf/kwai_runtime.sh
+
+## set directMem
+directMem="5g"
+# set heap size
+heapSize="5g"
+if [[ $KWS_SERVICE_NAME == *kop* ]]; then
+  directMem="5g"
+  heapSize="5g"
+  # install python kconf
+  pip3 install infra-kconf
+  # generate cluster config
+  python3 /home/web_server/bk-autorecovery/apps/bin/init_cluster_config.py
+fi
+
+export BOOKIE_MEM_OPTS="-Xms${heapSize} -Xmx${heapSize} -XX:MaxDirectMemorySize=${directMem} -XX:-UseNUMA"
+export BOOKIE_ROOT_LOG_APPENDER="ROLLINGFILE"
+export BOOKIE_LOG_DIR="/data/logs/$KWS_SERVICE_NAME"
+
+## set rocksdb jni dir, default is /tmp
+export ROCKSDB_SHAREDLIB_DIR="/home/web_server/bk-autorecovery/apps"
+
+numa='numactl --interleave all'
+which cgstart > /dev/null 2>&1
+if [[ $? != 0 ]]; then
+  echo "start bookkeeper without cgstart"
+  $numa bin/bookkeeper bk-autorecovery
+else
+  echo "start bookkeeper with cgstart"
+  $numa cgstart bin/bookkeeper bk-autorecovery
+fi
